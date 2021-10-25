f9ad4e09c8fd4967157c702203d508849ae21f68
Helena Oliveira <helena@informach.com.br>
2021-10-25 06:21:43 -0300

    feat: implement use case find user

--- src/infra/test/user_repository_spy.py
+++ src/infra/test/user_repository_spy.py
@@ -1,16 +1,14 @@
 from typing import List
-
-from src.data.interfaces import UserRepositoryInterface
 from src.domain.models import Users
 from src.domain.test import mock_users
 
 
-class UserRepositorySpy(UserRepositoryInterface):
+class UserRepositorySpy:
     """Spy to User Repository"""
 
     def __init__(self):
         self.insert_user_params = {}
-        self.select_user_param = {}
+        self.select_user_params = {}
 
     def insert_user(self, name: str, password: str) -> Users:
         """Spy to all the attributes"""
@@ -23,7 +21,7 @@ class UserRepositorySpy(UserRepositoryInterface):
     def select_user(self, user_id: int = None, name: str = None) -> List[Users]:
         """Spy to all the attributes"""
 
-        self.select_user_param["user_id"] = user_id
-        self.select_user_param["name"] = name
+        self.select_user_params["user_id"] = user_id
+        self.select_user_params["name"] = name
 
         return [mock_users()]
